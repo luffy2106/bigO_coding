2ff0b0b49a98a5c5f71e8561b49639faa57d54f2
DANG Trung Kien <tkdang@assystem.com>
2022-06-13 17:55:01 +0200

    update

--- Lecture5_BFS/BreadthFirstSearchShortestReach.py
+++ Lecture5_BFS/BreadthFirstSearchShortestReach.py
@@ -1,13 +1,8 @@
 from queue import Queue
+import sys
 
-MAX = 100
-V = None  # vertex
-E = None  # edge
-visited = [False for i in range(MAX)]
-path = [0 for i in range(MAX)]
-graph = [
-    [] for i in range(MAX)
-]  # i se tuong duong voi dinh, [i] se tuong duong voi dinh dc noi den chinh no
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(10 ** 6)  # Set maximum recursive
 
 
 def BFS(s):
@@ -30,36 +25,41 @@ def BFS(s):
 
 
 def getPathRecursion(s, f, cost):
-    cost_path = 0
     if s == f:
-        # print(cost, end=" ")
-        cost_path = cost
-        return cost_path
+        return cost
     else:
         if path[f] == -1:
-            # print("-1")
-            # cost = -1
             return -1
         else:
             cost = cost + 6
-            getPathRecursion(s, path[f], cost)
-            # print(f, end=" ")
+            return getPathRecursion(s, path[f], cost)
 
 
 Q = int(input())
 out_total = []
 for i in range(Q):
+    # MAX = 100
+    V = None  # vertex
+    E = None  # edge
     V, E = map(int, input().split())
+    MAX = V + 1
+    visited = [False for i in range(MAX)]
+    path = [0 for i in range(MAX)]
+    graph = [
+        [] for i in range(MAX)
+    ]  # i se tuong duong voi dinh, [i] se tuong duong voi dinh dc noi den chinh no
+
     for i in range(E):
         u, v = map(int, input().split())
         graph[u].append(v)
         graph[v].append(u)
     s = int(input())
+
     list_dest = [x for x in range(1, V + 1) if x != s]
     BFS(s)
     out = ""
     for dest in list_dest:
-        getPathRecursion(s, dest, 0)
+        # getPathRecursion(s, dest, 0)
         out = out + " " + str(getPathRecursion(s, dest, 0))
     out_total.append(out.strip())
 
