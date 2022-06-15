878609595770915d5974fa46a14db2868a81aad6
DANG Trung Kien <tkdang@assystem.com>
2022-06-15 18:18:23 +0200

    update

--- Lecture7_heap/Qheap1_solution2.py
+++ Lecture7_heap/Qheap1_solution2.py
@@ -24,21 +24,46 @@ def index_parent(i):
     return int((i - 1) / 2)
 
 
+# N = int(input())
+# h = []  # our heap
+# out = []
+# index_value = [h[i] for i in range(len(h))]
+# min = 0
+# for i in range(N):
+#     operation = input().split()
+#     operation = [int(x) for x in operation]
+#     if operation[0] == 1:
+#         heapq.heappush(h, operation[1])
+#     elif operation[0] == 2:
+#         while len(h) > 0 and h[0] == operation[1]:
+#             heapq.heappop(h)
+#     else:
+#         out.append(h[0])
+
+# for e in out:
+#     print(e)
+
+
 N = int(input())
 h = []  # our heap
 out = []
 index_value = [h[i] for i in range(len(h))]
 min = 0
+delete = False
 for i in range(N):
     operation = input().split()
     operation = [int(x) for x in operation]
     if operation[0] == 1:
         heapq.heappush(h, operation[1])
     elif operation[0] == 2:
-        while len(h) > 0 and h[0] == operation[1]:
+        if h[0] == operation[1]:
+            delete = True
+    elif operation[0] == 3:
+        if delete:
             heapq.heappop(h)
-    else:
-        out.append(h[0])
+            out.append(h[0])
+        else:
+            out.append(h[0])
 
 for e in out:
     print(e)
