6203b330ebd11852c8d726977f7b78ee653a0a45
TrungKien <kien.dangtrung.hust@gmail.com>
2022-06-20 23:20:20 +0200

    finish

--- Lecture2_AlgorithmicComplexity/GeorgeAndRound.py
+++ Lecture2_AlgorithmicComplexity/GeorgeAndRound.py
@@ -1,25 +1,26 @@
+"""
+Tự làm nhưng cần đọc hướng dẫn để hiểu yêu cầu của đề bài. 
+Nhận xét:
+- Nếu một bài toán có độ phức tạp Ai, nó cần ít nhất 1 dạng bài có độ phức tạp Bj>=Ai. Ta nên chọn giá trị
+Bj bé nhất có thể, để dành các dạng bài có độ phức tạp cao hơn cho các bài toán khó hơn.
+- Lưu ý là các bài toán và các dạng toán đều có độ phức tạp tăng dần
+
+"""
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-freq_a = [0] * (pow(10, 6) + 1)
-freq_b = [0] * (pow(10, 6) + 1)
-unique_a = 0
-unique_b_in_a = 0
-for a in A:
-    if freq_a[a] == 0:
-        unique_a += 1
-    freq_a[a] += 1
+
+j=0
+count = 0
+for i in range(N):
+    #find Bj such as Bj>=Ai, stop when found
+    while j < M:
+        if B[j] >= A[i]:
+            count+=1
+            j+=1
+            break
+        j+=1
+
+print(N-count)
 
 
-for b in B:
-    if freq_a[b] > 0:
-        if freq_b[b] == 0:
-            unique_b_in_a += 1
-        else:
-            continue
-    else:
-        unique_b_in_a += 1
-    freq_b[b] += 1
-
-
-print(unique_a - unique_b_in_a)
