b7533b203fd592e63e6e1bb0162c7b190fd7dd09
TrungKien <kien.dangtrung.hust@gmail.com>
2022-06-27 00:03:31 +0200

    start

---
+++ Lecture10_BellmanFord/Wormholes.py
@@ -0,0 +1,11 @@
+"""
+Chứng kiến dc vụ nổ big bang tương đương với việc quay về thời gian âm vô cùng. Vậy chỉ cần tìm được một chu trình âm, sau đó đi đi lại lại 
+chu trình âm đó thì sẽ có thể quay dc về thời gian âm vô cùng vì sau khi kết thúc 1 chu trình âm, thời gian bị giảm đi.
+=> xác định đồ thị tồn tại chu trình âm hay không => dùng BellmanFord
+"""
+
+class Edge:
+    
+
+
+
