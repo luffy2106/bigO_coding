781299b4bee581c41db441c3e1f3cfc1a868da2a
TrungKien <kien.dangtrung.hust@gmail.com>
2022-07-14 23:26:22 +0200

    update explain dijkstra optimize

--- Lecture8_Dijkstra/TravellingCost.py
+++ Lecture8_Dijkstra/TravellingCost.py
@@ -3,7 +3,7 @@ import heapq
 
 
 """
-Nhận xét :
+Nhận xét (*):
 Giả sử trong heap hiện tại có rất nhiều cặp có chung đích u nhưng khác trọng số w. Vì priority heap sẽ xét cặp có w nhỏ nhất,
 nên khi lấy cặp u,w này ra, nếu đường đi đến u khác với w, chứng tỏ cặp đang xét không nằm trong đường đi ngắn nhất đến u => bỏ qua
 
@@ -25,9 +25,7 @@ def Dijkstra(node_s):
         top = heapq.heappop(pq)
         u = top.id
         w = top.dist
-        if (dist[u] != w): #1
-        #Kiểm tra xem đường đi đến u có phải là phiên bản tốt nhất không, 
-        #nếu nó bằng w thì nó là phiên bản tốt nhất rồi, tiếp tục duyệt. Còn không thì nó ko phải là phiên bản tốt nhất => bỏ qua 
+        if (dist[u] != w): # (*)
             continue
         for neighbor in graph[u]:
             if neighbor.dist + w < dist[neighbor.id]:
