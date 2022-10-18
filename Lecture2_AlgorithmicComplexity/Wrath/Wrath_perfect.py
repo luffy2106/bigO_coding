0aada99f921a2d701e0d1c43b45274fe2e7ce13f
DANG Trung Kien <tkdang@assystem.com>
2022-10-18 13:29:30 +0200

    add comment Warth

--- Lecture2_AlgorithmicComplexity/Wrath/Wrath_perfect.py
+++ Lecture2_AlgorithmicComplexity/Wrath/Wrath_perfect.py
@@ -14,7 +14,7 @@ list_L = [int(x) for x in input().split()]
 total_kill = 0
 last_killed_index = N-1
 for i in range(len(list_L)-1, -1, -1): # For each loop j, we want to see how many people j can kill, if these people alreay be killed, we ignore them(based on the last people was killed in the previous loop)
-    if i > i - list_L[i]:
+    if i > i - list_L[i]: #The condition to make sure index i can kill at least one person 
         last_victim_of_i = max(0,i - list_L[i])
         if last_victim_of_i < last_killed_index:
             total_kill+= min(i, last_killed_index) - last_victim_of_i
